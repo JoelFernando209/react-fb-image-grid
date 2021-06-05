cfc2168e476bd22eab42b33e365d03337249d061
Joel Fernando Dominguez Tapia <joeldomingueztapia@gmail.com>
2021-06-04 20:19:56 -0700

    Delete react-bootstrap property

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Image, Modal, Grid, Row, Col } from "react-bootstrap";
 import Lightbox from "lightbox-react";
 import "lightbox-react/style.css"; // This only needs to be imported once in your app
 class ModalComponent extends Component {
